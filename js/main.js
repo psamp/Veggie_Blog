c4c9edfca509eb3a5aed599ba16fd391467f65bc
Princess Sampson <ps@kingonomics.com>
2015-06-02 21:38:06 -0400

    console logged all the post/timeline data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 console.log('This is cool!');
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+// $('.container').html(template.example({
+//   value: 'Hello, World!!'
+// }));
+
+$.getJSON( "http://private-44f6d-timeline6.apiary-mock.com/timeline").done( function (blogData) {
+
+    console.log(blogData);
+
+    blogData.timelines = blogData.timelines.forEach(function(posts) {
+
+      var title = posts.title;
+      
+      var timestamp = posts.timestamp;
+
+      var content = posts.content;
+      
+      var icon = posts.icon;
+      
+      var data = timestamp + "\n" + title + "\n" + content + "\n" + icon;
+      
+      console.log(data);
+    
+    });
+
+});
