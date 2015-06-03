d88b307a153becac10a5b655f4e56d42896d493c
Princess Sampson <ps@kingonomics.com>
2015-06-03 16:39:40 -0400

    installed bourbon + neat, Roboto font, started some css styling

--- js/main.js
+++ js/main.js
@@ -1,35 +1,12 @@
 console.log('This is cool!');
 
-// JD'S MOVIES JS
-
-// $.getJSON('movies.json').done(function(data){
-
-//   console.log('data', data);
-  
-//   data.movies = data.movies.map(function(movie){
-
-//     if(movie.release) {
-
-//       var date = moment(new Date(movie.release));
-
-//       movie.release = date.format('MMMM Do, \'YY');
-//     }
-  
-//   return movie;
-
-//   });
-
-//   $('.container').html(template.movies(data));
-
-// });
-
 $.getJSON( "http://private-44f6d-timeline6.apiary-mock.com/timeline").done(function (blogData) {
 
-  blogData = blogData.timelines.map(function(post) {
+  console.log(blogData);
+
+  blogData.timelines.forEach(function(post) {
 
     var title = post.title;
-
-    // post date + timeline
 
     var date = moment(post.timestamp);
       
@@ -37,11 +14,11 @@ $.getJSON( "http://private-44f6d-timeline6.apiary-mock.com/timeline").done(funct
 
     post.timestamp = date.format('M/D/YY');
 
-    var poststamp = time.format('HH:mm');
+    post.poststamp = time.format('HH:mm');
+
+    var poststamp = post.poststamp;
       
     var timestamp = post.timestamp;
-
-    // end postdate + timestamp
 
     var content = post.content;
       
@@ -49,14 +26,10 @@ $.getJSON( "http://private-44f6d-timeline6.apiary-mock.com/timeline").done(funct
       
     var consoleLogData = timestamp + "\n" + poststamp + "\n" + title + "\n" + content + "\n" + icon;
 
-    console.log(consoleLogData);
+    // console.log(consoleLogData);
 
     return post;
-
     });
 
-  console.log(blogData);
-
   $('.blogposts').html(template.blogpost(blogData));
-
 });
--- js/templates.js
+++ js/templates.js
@@ -2,20 +2,19 @@ this["template"] = this["template"] || {};
 this["template"]["blogpost"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "\n    <li>\n\n    <span class=\"tstamp\">"
+  return "<div class=\"sidebar\">\n\n  <div class=\"timestamp\">\n    <span class=\"tstamp\">\n    "
     + alias3(((helper = (helper = helpers.timestamp || (depth0 != null ? depth0.timestamp : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"timestamp","hash":{},"data":data}) : helper)))
-    + "</span>\n\n    <span class=\"pstamp\">"
+    + "\n    </span>\n\n    <span class=\"pstamp\">\n    "
     + alias3(((helper = (helper = helpers.poststamp || (depth0 != null ? depth0.poststamp : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"poststamp","hash":{},"data":data}) : helper)))
-    + "</span>\n\n    <span class=\"icon\"><i class=\"fa "
+    + "\n    </span>\n  </div>\n\n  <div class=\"icon\">\n    <div class=\"iconitself\">\n      <span class=\"iconitsef\"><i class=\"fa "
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\"></i></span>\n\n\n    <h1>"
+    + "\"></i></span>\n    </div>\n  </div>\n\n</div>\n\n<div class=\"post\">\n\n  <div class=\"title\">\n    <h1>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h1>\n\n    <p>"
+    + "</h1>\n  </div>\n\n  <p class =\"posttext\">"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</p>\n\n    </li>\n\n";
+    + "</p>\n  \n</div>\n\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "\n"
-    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.posts : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.timelines : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
